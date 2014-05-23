d4a45374afb7189d47bfeb82f6fe2a3520677773
Ted <tedkwasnik@gmail.com>
2014-05-22 22:06:01 -0400

    comments

--- server.rb
+++ server.rb
@@ -2,17 +2,33 @@ require 'sinatra'
 require 'uri'
 require 'csv'
 
-def get_articles
-  articles = []
-  CSV.foreach('articles.csv', headers: true) do |row|
-    articles << row.to_hash
+def get_comment(article_id)
+  comments = []
+  CSV.foreach('comments.csv', headers: true) do |row|
+    comments << row.to_hash if row["article_id"] == article_id
   end
-  articles.reverse
+  comments.reverse
 end
 
-def post_article(title, url, descriptor,calander_time,clock_time)
+def get_articles
+  all_articles = []
+  CSV.foreach('articles.csv', headers: true) do |row|
+    article = row.to_hash
+    article["comments"] = get_comment(row["article_id"])
+    all_articles << article
+  end
+  all_articles.reverse
+end
+
+def post_article(id, title, url, descriptor,calander_time,clock_time)
   CSV.open('articles.csv', 'a') do |csv|
-    csv << [title, url, descriptor,calander_time,clock_time]
+    csv << [id, title, url, descriptor,calander_time,clock_time]
+  end
+end
+
+def post_comment(id, comment, time)
+  CSV.open('comments.csv', 'a') do |csv|
+    csv << [id, comment, time]
   end
 end
 
@@ -22,16 +38,20 @@ def url_exists?(url)
   end
 end
 
-
-get "/"  do
-  @articles = get_articles
-  erb :main
-end
-
 get "/submit"  do
   @populated_info = {}
   @missing_info = []
   erb :submit
+end
+
+get "/addcomment/:article_id"  do
+  @article_info = get_articles.reverse[params[:article_id].to_i]
+  erb :addcomment
+end
+
+post "/addcomment"  do
+  post_comment(params[:article_id], params[:comment], params[:time])
+  redirect "/"
 end
 
 post "/submit"  do
@@ -59,11 +79,16 @@ post "/submit"  do
   end
 
   if checks_failed == 0
-    post_article(params[:title],params[:url],URI.encode(params[:descriptor]),params[:calander_time],params[:clock_time])
+    id = get_articles.length
+    post_article(id, params[:title],params[:url],URI.encode(params[:descriptor]),params[:calander_time],params[:clock_time])
     redirect "/"
   else
     erb :submit
   end
 #
+end
 
+get "/*"  do
+  @articles = get_articles
+  erb :main
 end
